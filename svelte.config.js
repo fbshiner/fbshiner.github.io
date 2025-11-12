2d578f63b8301f8e9d19b47c19e723bb5d38ee73
Jeff Blake <jtblake@middlebury.edu>
2025-11-11 20:18:21 -0600

    plz

--- svelte.config.js
+++ svelte.config.js
@@ -36,7 +36,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH // remove when frankie makes their own github account and we transfer the repo
+			base: process.argv.includes('dev') ? '' : '/frankie-site' // process.env.BASE_PATH // remove when frankie makes their own github account and we transfer the repo
 		}
 	}
 };
