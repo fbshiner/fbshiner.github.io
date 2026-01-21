00243b73a67af765569a6bc4e4d3fb405b0f4d5e
Jeff Blake <jtblake@middlebury.edu>
2026-01-20 18:20:39 -0600

    Change text to test

--- src/routes/misc/data.js
+++ src/routes/misc/data.js
@@ -3,36 +3,36 @@ export const posts = [
         slug: "test",
         title: "fun1",
         src: "test.png",
-        description: "goobagaba"
+        description: "test"
     },
     {
         slug: "test",
         title: "fun2",
         src: "test.png",
-        description: "goobagaba"
+        description: "test"
     },
     {
         slug: "test",
         title: "fun3",
         src: "test.png",
-        description: "goobagaba"
+        description: "test"
     },
     {
         slug: "test",
         title: "fun4",
         src: "test.png",
-        description: "goobagaba"
+        description: "test"
     },
     {
         slug: "test",
         title: "fun5",
         src: "test.png",
-        description: "goobagaba"
+        description: "test"
     },
     {
         slug: "test",
         title: "fun6",
         src: "test.png",
-        description: "goobagaba"
+        description: "test"
     },
 ]
