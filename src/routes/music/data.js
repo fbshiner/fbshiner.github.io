427f338f07ed71872be027961bc34a9cdc58c480
Jeff Blake <jtblake@middlebury.edu>
2026-02-01 18:59:08 -0600

    Added data.js template

--- src/routes/music/data.js
+++ src/routes/music/data.js
@@ -1,38 +1,11 @@
 export const posts = [
+    /* image format, insert below the * / after the },
     {
-        slug: "test",
-        title: "fun1",
-        src: "test.png",
-        description: "test"
+        "slug": "slug_name", // name at the end of the url e.g. fbshiner.github.io/knitting/slug_name
+        "title": "Image Title",
+        "src": "name_of_image_in_assets.jpg", // could always be png or jpeg, etc.
+        "description": "Description"
     },
-    {
-        slug: "test",
-        title: "fun2",
-        src: "test.png",
-        description: "test"
-    },
-    {
-        slug: "test",
-        title: "fun3",
-        src: "test.png",
-        description: "test"
-    },
-    {
-        slug: "test",
-        title: "fun4",
-        src: "test.png",
-        description: "test"
-    },
-    {
-        slug: "test",
-        title: "fun5",
-        src: "test.png",
-        description: "test"
-    },
-    {
-        slug: "test",
-        title: "fun6",
-        src: "test.png",
-        description: "test"
-    },
+    */
+
 ]
