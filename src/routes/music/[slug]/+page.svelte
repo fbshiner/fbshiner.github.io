21b9610bb5f586592594b31dbe367ef83008d7f3
Jeff Blake <jtblake@middlebury.edu>
2026-02-08 12:53:31 -0600

    Fix music import to use meta.glob

--- src/routes/music/[slug]/+page.svelte
+++ src/routes/music/[slug]/+page.svelte
@@ -8,6 +8,11 @@
         import: 'default'
     });
 
+    const musicFiles = import.meta.glob("$lib/assets/music/*.{mp3,wav,ogg,flac}", {
+        eager: true,
+        import: 'default'
+    });
+
     const title = data.post.title;
     const description = data.post.description;
     const link = data.post.link;
@@ -15,7 +20,9 @@
 
     const srcInfo = data.post.src.split(".");
     const path = `/src/lib/assets/music/${srcInfo[0]}.${srcInfo[1]}`;
-    const musicPath = `/src/lib/assets/music/${data.post.musicFile}`;
+
+    const musicInfo = data.post.musicFile.split(".");
+    const musicPath = `/src/lib/assets/music/${musicInfo[0]}.${musicInfo[1]}`;
 
     const pageContext = {
         "image": images[path],
@@ -23,7 +30,7 @@
         "description": description,
         "link": link,
         "linkText": linkText,
-        "musicFile": musicPath,
+        "musicFile": musicFiles[musicPath],
     }
     
 </script>
