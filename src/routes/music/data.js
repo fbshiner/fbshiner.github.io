bb66420fd271119549444a31330a22811cec1982
Jeff Blake <jtblake@middlebury.edu>
2026-02-16 18:29:45 -0600

    Demo fix

--- src/routes/music/data.js
+++ src/routes/music/data.js
@@ -6,7 +6,7 @@ export const posts = [
         "src": "name_of_image_in_assets.jpg", // could also be png or jpeg
         "description": "Description",
         "alt": "Alt text for image",
-        "link": "link.com", // optional, shows a link
+        "link": "link.com", // optional, shows a link (MUST BE PREFIXED WITH HTTPS://)
         "linkText": "Text you click on to access the link", // optional, if not filed and out there is a link, the default text is "link"
         "musicFile": "music.mp3", // optional, provides an audio player at the bottom of the screen
     },
@@ -17,7 +17,7 @@ export const posts = [
         "src": "d-test.jpg",
         "description": "Aut consequuntur quas sapiente modi ea voluptatem. Nemo dolor itaque tempora rerum qui eos magnam. Ut ducimus voluptatibus laborum id ipsa id. Aut est tempora neque accusamus iste qui. Voluptate et neque eum architecto qui. Ipsa cupiditate et et voluptate dicta. Aliquam beatae ea facere dolorem. Sit nihil et aut nihil non facilis.",
         "alt": "Dish towel",
-        "link": "soundcloud.com",
+        "link": "https://www.soundcloud.com", 
         "linkText": "CLICK HERE FOR MONEY! NOW!!!!",
         "musicFile": "s-test.mp3",
     }
